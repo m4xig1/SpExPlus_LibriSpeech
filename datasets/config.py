9194806f7bc1db1b778f293f251142890a718333
m4xig1 <m4xig1@yandex.ru>
2024-03-23 15:00:18 +0300

    dataset config fixed

--- datasets/config.py
+++ datasets/config.py
@@ -2,8 +2,26 @@ config_dataloader = {
     "logging_name": "dataloader",
     "path_to_train": "/home/m4xig1/speaker_extraction_SpEx/libri_dataset/mix/train/",
     "path_to_val": "/home/m4xig1/speaker_extraction_SpEx/libri_dataset/mix/val/",
-    "train": {"batch_size": 4, "num_workers": 4, "create_index" : True, "index_path": "/home/m4xig1/speaker_extraction_SpEx/datasets/"}, # batch size = 16?
-    "val": {"batch_size": 4, "num_workers": 4, "create_index" : True},
-    "test": {"batch_size": 4, "num_workers": 4, "create_index" : True},
+    "train": {
+        "batch_size": 4,
+        "num_workers": 4,
+        "create_index": False,
+        "index_path": "/home/m4xig1/speaker_extraction_SpEx/datasets/",
+        "logging_name": "train dataloader",
+    },  # batch size = 16?
+    # "val": {
+    #     "batch_size": 4,
+    #     "num_workers": 4,
+    #     "create_index": True,
+    #     "index_path": "/home/m4xig1/speaker_extraction_SpEx/datasets/",
+    #     "logging_name": "val dataloader",
+    # },
+    "test": {
+        "batch_size": 4,
+        "num_workers": 4,
+        "create_index": True,
+        "index_path": "/home/m4xig1/speaker_extraction_SpEx/datasets/",
+        "logging_name": "test dataloader",
+    },
     "sample_rate": 16000,
 }
