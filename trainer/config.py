10525d90f35f936ca04eb9395d4f06cea38243c1
m4xig1 <m4xig1@yandex.ru>
2024-04-05 01:47:37 +0300

    idk...

--- trainer/config.py
+++ trainer/config.py
@@ -12,8 +12,8 @@ config_trainer = {
     },
     "epoch_len": 1000,
     "nCheckpoints": 10,
-    "checkpoint_path": "/home/m4xig1/speaker_extraction_SpEx/checkpoints/",
-    # "checkpoint_path": "/kaggle/working/SpExPlus_LibriSpeech/model/",
+    # "checkpoint_path": "/home/m4xig1/speaker_extraction_SpEx/checkpoints/",
+    "checkpoint_path": "/kaggle/working/SpExPlus_LibriSpeech/model/",
     "device": "cuda", 
     "no_improvment": 6,  # steps before stopping the training
 }
