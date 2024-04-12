3f8efb3706be7cee1797e8bcc86e8260316eb879
m4xig1 <m4xig1@yandex.ru>
2024-04-12 23:27:35 +0300

    kaggle fine tune

--- datasets/config.py
+++ datasets/config.py
@@ -5,16 +5,16 @@ cur_dir = os.getcwd()
 
 config_dataloader = {
     "logging_name": "dataloader",
-    # "path_to_train": "/kaggle/input/librispeech-mixes/train-clean-100-mixed/",
-    # "path_to_val": "/kaggle/input/librispeech-mixes/test-clean-mixed/",
-    "path_to_train": cur_dir + "/librispeech-mixes/train-clean-100-mixed/",
-    "path_to_val": cur_dir + "/librispeech-mixes/test-clean-mixed/",
+    "path_to_train": "/kaggle/input/librispeech-mixes/train-clean-100-mixed/",
+    "path_to_val": "/kaggle/input/librispeech-mixes/test-clean-mixed/",
+    # "path_to_train": cur_dir + "/librispeech-mixes/train-clean-100-mixed/",
+    # "path_to_val": cur_dir + "/librispeech-mixes/test-clean-mixed/",
     "train": {
         "batch_size": 2,
         "num_workers": 4,
         "create_index": True,
-        "index_path": cur_dir + "/datasets/",
-        # "index_path": "/kaggle/working/SpExPlus_LibriSpeech/datasets/",
+        # "index_path": cur_dir + "/datasets/",
+        "index_path": "/kaggle/working/SpExPlus_LibriSpeech/datasets/",
         "logging_name": "train dataloader",
         "max_len": None,  # number of samples in dataset
     },
@@ -22,8 +22,8 @@ config_dataloader = {
         "batch_size": 1,
         "num_workers": 4,
         "create_index": True,
-        "index_path": cur_dir + "/datasets/",
-        # "index_path": "/kaggle/working/SpExPlus_LibriSpeech/datasets/",
+        # "index_path": cur_dir + "/datasets/",
+        "index_path": "/kaggle/working/SpExPlus_LibriSpeech/datasets/",
         "logging_name": "test dataloader",
         "max_len": 500,
     },
--- trainer/config.py
+++ trainer/config.py
@@ -4,6 +4,31 @@ import os
 cur_dir = os.getcwd()
 
 
+# config_trainer = {
+#     "logger": {"level": logging.INFO},
+#     "optimizer": {"lr": 1e-3, "weight_decay": 1e-5},
+#     "lr_scheduler": {  # Reduce on plateau
+#         "mode": "min",
+#         "factor": 0.5,
+#         "patience": 2,
+#         "min_lr": 1e-6,
+#         "verbose": False,
+#         "requires_loss": True,
+#         "epoch_based": True,
+#     },
+#     "epoch_len": 2000,
+#     # "checkpoint_path": "/home/m4xig1/speaker_extraction_SpEx/checkpoints/",
+#     # "checkpoint_path": "/kaggle/working/SpExPlus_LibriSpeech/model/",
+#     "checkpoint_path": cur_dir + "/model/",
+#     "device": "cuda",
+#     "no_improvment": 20,  # epochs before stopping the training if val_loss is not decending
+#     "epochs": 100,
+#     "save_period": 2,
+#     "monitor": "min val_loss",
+#     "max_grad_norm": 256,
+#     "resume_path": None
+# }
+
 config_trainer = {
     "logger": {"level": logging.INFO},
     "optimizer": {"lr": 1e-3, "weight_decay": 1e-5},
@@ -16,14 +41,16 @@ config_trainer = {
         "requires_loss": True,
         "epoch_based": True,
     },
-    "epoch_len": 5000,
+    "epoch_len": 2000,
     # "checkpoint_path": "/home/m4xig1/speaker_extraction_SpEx/checkpoints/",
-    # "checkpoint_path": "/kaggle/working/SpExPlus_LibriSpeech/model/",
-    "checkpoint_path": cur_dir + "/model/",
+    "checkpoint_path": "/kaggle/working/SpExPlus_LibriSpeech/model/",
+    # "checkpoint_path": cur_dir + "/model/",
     "device": "cuda",
     "no_improvment": 20,  # epochs before stopping the training if val_loss is not decending
     "epochs": 100,
     "save_period": 2,
     "monitor": "min val_loss",
     "max_grad_norm": 256,
+    "resume_path": "/kaggle/input/spex-best-shot/pytorch/12epochs/1"
 }
+
